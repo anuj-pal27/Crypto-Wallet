8ecc5170e2ed0a0182c04f662f35e2868438d2db
be-a-guptaji <aryanbaadlas@gmail.com>
2024-10-06 14:09:32 +0530

    Removed Confirmation so that it works properly

--- src/SignMessage.jsx
+++ src/SignMessage.jsx
@@ -4,41 +4,51 @@ import bs58 from "bs58";
 import { useState } from "react";
 
 export function SignMessage() {
+  const wallet = useWallet();
   const { publicKey, signMessage } = useWallet();
   const [message, setMessage] = useState("");
 
   async function sign() {
-    if (!publicKey) throw new Error("Wallet not connected!");
-    if (!signMessage)
+    if (!publicKey) {
+      throw new Error("Wallet not connected!");
+    }
+    if (!signMessage) {
       throw new Error("Wallet does not support message signing!");
+    }
     const encodedMessage = new TextEncoder().encode(message);
     const signature = await signMessage(encodedMessage);
 
-    if (!ed25519.verify(signature, encodedMessage, publicKey.toBytes()))
+    if (!ed25519.verify(signature, encodedMessage, publicKey.toBytes())) {
       throw new Error("Message signature invalid!");
-    alert("success", `Message signature: ${bs58.encode(signature)}`);
+    }
+    alert("Success", `Message signature: ${bs58.encode(signature)}`);
   }
 
-    return (
-      <>
-        <h1 className="text-3xl font-semibold text-center my-8 bg-black">
+  return (
+    <>
+      <h1 className="text-3xl font-semibold text-center my-8 bg-black">
+        Sign Message
+      </h1>
+      <div className="flex flex-col gap-4 bg-black">
+        <input
+          value={message}
+          type="text"
+          placeholder="Message"
+          onChange={(e) => setMessage(e.target.value)}
+          className="border border-gray-600 rounded-lg h-10 px-4"
+        />
+        <button
+          onClick={sign}
+          className="rounded-lg h-12 px-2 bg-[#512da8] disabled:bg-gray-700"
+        >
           Sign Message
-        </h1>
-        <div className="flex flex-col gap-4 bg-black">
-          <input
-            value={message}
-            type="text"
-            placeholder="Message"
-            onChange={(e) => setMessage(e.target.value)}
-            className="border border-gray-600 rounded-lg h-10 px-4"
-          />
-          <button
-            onClick={sign}
-            className="rounded-lg h-12 px-2 bg-[#512da8] disabled:bg-gray-700"
-          >
-            Sign Message
-          </button>
-        </div>
-      </>
-    );
+        </button>
+        {!wallet.connected && (
+          <p className="mt-2 text-center bg-black">
+            Please connect your wallet
+          </p>
+        )}
+      </div>
+    </>
+  );
 }
